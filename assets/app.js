12e3f9c0ae145195a6b2918fb56560447bcf6167
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-01-31 00:30:53 -0500

    add pic

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+$(document).ready(()=>{
+
+    $('#project-1').hover(()=>{
+        $('#p1Description').fadeToggle( "slow", "linear" );
+    })
+    $('#project-2').hover(()=>{
+        $('#p2Description').fadeToggle( "slow", "linear" );
+    })
+    $('#project-3').hover(()=>{
+        $('#p3Description').fadeToggle( "slow", "linear" );
+    })
+    $('#project-4').hover(()=>{
+        $('#p4Description').fadeToggle( "slow", "linear" );
+    })
+    $('#project-5').hover(()=>{
+        $('#p5Description').fadeToggle( "slow", "linear" );
+    })
+    $('#project-6').hover(()=>{
+        $('#p6Description').fadeToggle( "slow", "linear" );
+    })
+
+})
